9a8f410bf89e0ed1639db464a75e3763c831455f
TAsenova <tasenova@mentormate.com>
2024-11-08 14:40:27 +0200

    loader fn

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,5 @@
 import type { MetaFunction } from "@remix-run/node";
+import { json, useLoaderData } from "@remix-run/react";
 
 export const meta: MetaFunction = () => {
   return [
@@ -11,10 +12,26 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const response = await fetch(
+    "https://jsonplaceholder.typicode.com/posts?_limit=10"
+  );
+  const data: { id: string; title: string; body: string; userId: string }[] =
+    await response.json();
+  console.log(data);
+  return json({ blogs: data });
+};
+
 export default function Index() {
+  const { blogs } = useLoaderData<typeof loader>();
   return (
-    <div className="flex h-screen items-center justify-center">
-      <h1>Hi</h1>
+    <div className="grid grid-cols-2 lg:grid-cols-4 gap-5">
+      {blogs.map((blog) => (
+        <div className="p-3 shadow-md rounded-sm cursor-pointer" key={blog.id}>
+          <h2 className="font-bold uppercase pb-2">{blog.title}</h2>
+          <p>{blog.body}</p>
+        </div>
+      ))}
     </div>
   );
 }
