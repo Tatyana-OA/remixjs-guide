c91d85402edbcf27e69b2218d06aed1d878fce89
TAsenova <tasenova@mentormate.com>
2024-11-08 15:31:21 +0200

    forms

--- app/routes/blogs.$blogId.tsx
+++ app/routes/blogs.$blogId.tsx
@@ -1,5 +1,5 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { Link, useLoaderData } from "@remix-run/react";
+import { ActionFunction, LoaderFunctionArgs } from "@remix-run/node";
+import { Form, Link, useLoaderData } from "@remix-run/react";
 
 export const loader = async ({ params }: LoaderFunctionArgs) => {
   const { blogId } = params;
@@ -7,7 +7,21 @@ export const loader = async ({ params }: LoaderFunctionArgs) => {
     `https://jsonplaceholder.typicode.com/posts/${blogId}`
   );
   const data = await response.json();
-  console.log(data);
+  return data;
+};
+
+export const action: ActionFunction = async ({ request, params }) => {
+  const formData = await request.formData();
+  const title = formData.get("title");
+  const body = formData.get("body");
+  console.log(params);
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.blogId}`,
+    { body: JSON.stringify({ title, body, id: params.blogId }), method: "PUT" }
+  );
+
+  const data = await response.json();
+
   return data;
 };
 
@@ -18,13 +32,36 @@ const Blog = () => {
       key={id}
       className="max-w-4xl mx-auto p-6 bg-white shadow-lg rounded-lg"
     >
-      <Link to="/" className="italic font-semibold">
+      <Link
+        to="/"
+        className="italic font-semibold text-gray-600 hover:text-gray-900"
+      >
         Home
       </Link>
       <h2 className="text-2xl font-bold text-gray-900 my-4 capitalize">
         {title}
       </h2>
       <p className="text-gray-700 leading-relaxed">{body}</p>
+
+      <Form className="mt-6" method="put">
+        <input
+          name="title"
+          type="text"
+          placeholder="Update your post title"
+          className="w-full p-3 mb-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500"
+        />
+        <textarea
+          name="body"
+          placeholder="Update your post body"
+          className="w-full p-3 mb-4 border border-gray-300 rounded-lg shadow-sm focus:outline-none focus:ring-2 focus:ring-gray-500 focus:border-gray-500"
+        />
+        <button
+          type="submit"
+          className="w-full p-3 bg-gray-800 text-white rounded-lg hover:bg-gray-700 transition-colors focus:outline-none focus:ring-2 focus:ring-gray-500"
+        >
+          Update
+        </button>
+      </Form>
     </div>
   );
 };
